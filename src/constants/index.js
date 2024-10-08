419992dbd39340ce66abc51c17b02f5de2c1dbf6
Elena <ewlen04@gmail.com>
2024-10-08 16:27:29 +0200

    logo design adjustments and styles modificiations

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,38 +9,45 @@ import {
     yellowImg,
   } from "../utils";
   
-  export const navLists = ["Models", "About", "Contact", "Equipment" , "Filament"];
+  export const navLists = ["Models", "About", "Contact", "Equipment" , "Filament", "Electronics"];
   
   export const hightlightsSlides = [
     {
       id: 1,
       textLists: [
-        "Enter A17 Pro.",
-        "Game‑changing chip.",
-        "Groundbreaking performance.",
+        "You wanna try something new?",
+        " Canonc game is a fun way to pass the time with friens or family",
+        "It can also be a great gift or an addition to your 3D models collection.",
       ],
       video: highlightFirstVideo,
       videoDuration: 4,
     },
     {
       id: 2,
-      textLists: ["Titanium.", "So strong. So light. So Pro."],
+      textLists: ["This Mario model represents a", 
+        "innovative concept of a toy and a phont holder.",
+        "Six colors that smothly align in this model.",
+      ],
       video: highlightSecondVideo,
       videoDuration: 5,
     },
     {
       id: 3,
       textLists: [
-        "iPhone 15 Pro Max has the",
-        "longest optical zoom in",
-        "iPhone ever. Far out.",
+        "Notable model",
+        "with personal photo that can be changed.",
+        "Unique point includes modifable concept with possibility to add more pieces.",
       ],
       video: highlightThirdVideo,
       videoDuration: 2,
     },
     {
       id: 4,
-      textLists: ["All-new Action button.", "What will yours do?."],
+      textLists: [
+        "Lookig for something one-of-the kind?",
+        "This 3D bottle can be charged by USB cable or battery light bulbs.",
+        "Insert photo can be changed." 
+      ],
       video: highlightFourthVideo,
       videoDuration: 3.63,
     },
@@ -49,25 +56,25 @@ import {
   export const models = [
     {
       id: 1,
-      title: "iPhone 15 Pro in Natural Titanium",
+      title: "Pirate Cannon Game.",
       color: ["#8F8A81", "#ffe7b9", "#6f6c64"],
       img: yellowImg,
     },
     {
       id: 2,
-      title: "iPhone 15 Pro in Blue Titanium",
+      title: "Mario Phone Holder",
       color: ["#53596E", "#6395ff", "#21242e"],
       img: blueImg,
     },
     {
       id: 3,
-      title: "iPhone 15 Pro in White Titanium",
+      title: "Lithophane wagon",
       color: ["#C9C8C2", "#ffffff", "#C9C8C2"],
       img: whiteImg,
     },
     {
       id: 4,
-      title: "iPhone 15 Pro in Black Titanium",
+      title: "Bottle of Champagne - Lithophane holder",
       color: ["#454749", "#3b3b3b", "#181819"],
       img: blackImg,
     },
--- src/utils/index.js
+++ src/utils/index.js
@@ -20,10 +20,10 @@ import replay from "/assets/images/replay.svg";
 import play from "/assets/images/play.svg";
 import pause from "/assets/images/pause.svg";
 
-import yellow from "/assets/images/yellow.jpg";
-import blue from "/assets/images/blue.jpg";
-import white from "/assets/images/white.jpg";
-import black from "/assets/images/black.jpg";
+import yellow from "/assets/images/yellow.png";
+import blue from "/assets/images/blue.png";
+import white from "/assets/images/white.png";
+import black from "/assets/images/black.png";
 import explore1 from "/assets/images/explore1.jpg";
 import explore2 from "/assets/images/explore2.jpg";
 import chip from "/assets/images/chip.jpeg";
