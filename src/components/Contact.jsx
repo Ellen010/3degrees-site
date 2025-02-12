80cb39a5ec8d51795110e3ab248c38aebe646a87
Elena <ewlen04@gmail.com>
2025-02-12 14:45:19 +0100

    bugs corrections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,12 +5,12 @@ import { animateWithGsap } from '../utils/animations';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
-const backendUrl = "http://192.168.100.197:5000";
+// const backendUrl = "http://192.168.100.197:5000";
 
 const Contact = () => {
-  const formRef = useRef(null);
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-  const [msgSend, setMsgSend] = useState(false);
+  // const formRef = useRef(null);
+  // const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  // const [msgSend, setMsgSend] = useState(false);
 
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, y: 0, duration: 1 });
@@ -26,41 +26,41 @@ const Contact = () => {
   
 
   
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  // const handleChange = (e) => {
+  //   setFormData({ ...formData, [e.target.name]: e.target.value });
+  // };
 
-  const handleSendMessage = async (e) => {
-    e.preventDefault(); 
+  // const handleSendMessage = async (e) => {
+  //   e.preventDefault(); 
 
-    console.log("Sending message:", formData);
+  //   console.log("Sending message:", formData);
 
-    try {
-      const response = await fetch(`${backendUrl}/email/contact`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          name: formData.name,
-          email: formData.email,
-          message: formData.message,
-        }),
-      });
+  //   try {
+  //     const response = await fetch(`${backendUrl}/email/contact`, {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({
+  //         name: formData.name,
+  //         email: formData.email,
+  //         message: formData.message,
+  //       }),
+  //     });
 
-      const data = await response.json();
-      console.log("Response:", data);
+  //     const data = await response.json();
+  //     console.log("Response:", data);
 
-      if (response.ok) {
-        setMsgSend(true);
-        alert("Message sent successfully!");
-        setFormData({ name: '', email: '', message: '' }); 
-      } else {
-        alert("Failed to send message. Please try again.");
-      }
-    } catch (error) {
-      console.error("Error sending message:", error);
-      alert("An error occurred. Please try again later.");
-    }
-  };
+  //     if (response.ok) {
+  //       setMsgSend(true);
+  //       alert("Message sent successfully!");
+  //       setFormData({ name: '', email: '', message: '' }); 
+  //     } else {
+  //       alert("Failed to send message. Please try again.");
+  //     }
+  //   } catch (error) {
+  //     console.error("Error sending message:", error);
+  //     alert("An error occurred. Please try again later.");
+  //   }
+  // };
 
   return (
     <section id="contact" className="w-screen overflow-hidden h-full common-padding bg-zink">
@@ -89,7 +89,7 @@ const Contact = () => {
               <p className="hiw-text">We look forward to connecting with you!</p>
               <p className="hiw-text">Feel free to customize it further to suit your needs!</p>
       </div>
-        <form ref={formRef} onSubmit={handleSendMessage} className="g_fadeIn p-20 flex flex-col max-w-lg mx-auto">
+        {/* <form ref={formRef} onSubmit={handleSendMessage} className="g_fadeIn p-20 flex flex-col max-w-lg mx-auto">
           <label className="mb-2">Name:</label>
           <input 
             name="name" 
@@ -121,7 +121,7 @@ const Contact = () => {
           ></textarea>
 
           <button type="submit" className="msg-btn g_fadeIn p:40">Send Message</button>
-        </form>
+        </form> */}
       </div>
     </section>
   );
