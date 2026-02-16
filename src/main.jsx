c452a16a267d25a26f3eef25d535292c040659fa
Elena WELTY <157579304+Ellen010@users.noreply.github.com>
2026-02-16 18:04:03 -0500

    Integrate Auth0Provider in main.jsx
    Wrap App component with Auth0Provider for authentication.

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 
 Sentry.init({
   dsn: "https://d674932a77e6d9b9ced1190d70fd4691@o4506876178464768.ingest.us.sentry.io/4506876181151744",
   integrations: [
     Sentry.browserTracingIntegration(),
-    // Sentry.metrics.metricsAggregatorIntegration(),
-    // Sentry.reactRouterV6BrowserTracingIntegration({
-    //   useEffect: React.useEffect,
-    // }),
     Sentry.replayIntegration({
       maskAllText: false,
       blockAllMedia: false,
@@ -25,8 +22,14 @@ Sentry.init({
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+    <React.StrictMode>
+    <Auth0Provider
+        domain="YOUR_AUTH0_DOMAIN"
+        clientId="YOUR_AUTH0_CLIENT_ID"
+        authorizationParams={{ redirect_uri: window.location.origin }}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
-)
+);
 
