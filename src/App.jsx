0caa473b07a4fe33a8d0a9fd0ca89f169cd3ec8f
Elena WELTY <157579304+Ellen010@users.noreply.github.com>
2026-02-16 18:24:24 -0500

    Refactor App component structure and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import React from "react"; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 import Navbar from "./components/Navbar";
 import Canon from "./components/Canon";
 import Contact from "./components/Contact";
@@ -11,42 +16,67 @@ import Highlights from "./components/Highlights";
 import HowItWorks from "./components/HowItWorks";
 import Model from "./components/Model";
 import Software from "./components/Software";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import * as Sentry from "@sentry/react";
+import ClientSpace from "./components/ClientSpace.jsx";
+import AuthButtons from "./components/AuthButtons.jsx";
+import "./index.css";
+
+
 
 function App() {
   return (
-    <Router>
-      <main className="bg-black">
-        <Navbar />
+      <Router>
+        <div className="bg-black min-h-screen text-white">
+          {/* Navbar + Auth Buttons */}
+          <Navbar />
+          <nav className="p-4 flex justify-end">
+            <AuthButtons />
+          </nav>
 
-        <Routes>
-          <Route 
-            path="/" 
-            element={
-              <>
-                <Canon />
-                <Highlights />
+          {/* Routes */}
+          <Routes>
+            <Route
+                path="/"
+                element={
+                  <>
+                    <Canon />
+                    {/* <Highlights />
+                <ClientSpace />
                 <Model />
                 <Features />
                 <HowItWorks />
-                <Footer />
-              </>
-            } 
-          />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/designs" element={<Designs />} />
-          <Route path="/equipment" element={<Equipment />} />
-          <Route path="/features" element={<Features />} />
-          <Route path="/filament" element={<Filament />} />
-          <Route path="/highlights" element={<Highlights />} />
-          <Route path="/how-it-works" element={<HowItWorks />} />
-          <Route path="/model" element={<Model />} /> 
-          <Route path="/software" element={<Software />} /> 
-        </Routes>
-      </main>
-    </Router>
+                <Footer /> */}
+                  </>
+                }
+            />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/designs" element={<Designs />} />
+            <Route path="/equipment" element={<Equipment />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/filament" element={<Filament />} />
+            <Route path="/highlights" element={<Highlights />} />
+            <Route path="/how-it-works" element={<HowItWorks />} />
+            <Route path="/model" element={<Model />} />
+            <Route path="/software" element={<Software />} />
+            <Route path="/client-space" element={<ClientSpace />} />
+          </Routes>
+
+          {/* Footer */}
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
-export default Sentry.withProfiler(App);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <Auth0Provider
+          domain="YOUR_AUTH0_DOMAIN"
+          clientId="YOUR_AUTH0_CLIENT_ID"
+          authorizationParams={{ redirect_uri: window.location.origin }}
+      >
+        {Sentry.withProfiler(<App />)}
+      </Auth0Provider>
+    </React.StrictMode>
+);
+
+export default App;
