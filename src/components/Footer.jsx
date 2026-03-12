c76b5139cbe512ebed18c504f6789bd0b0b0079e
Elena WELTY <157579304+Ellen010@users.noreply.github.com>
2026-03-12 17:20:47 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
         <div className="bg-neutral-700 my-5 h-[1px] w-full" />
 
         <div className="flex md:flex-row flex-col md:items-center justify-between">
-          <p className="txt-sm ">Copright @ 2024 3Degrees Inc. All rights reserved.</p>
+          <p className="txt-sm ">Copyright @ 2026 3Degrees Inc. All rights reserved.</p>
           <div className="flex">
             {footerLinks.map((link, i) => (
               <p key={link} className="font-semibold text-gray text-xs">
